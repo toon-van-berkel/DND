f36da2abd25bf78f7bf1c8d70a7ad8085aa11fc6
Toon-van-Berkel <toonvanberkel203@gmail.com>
2026-02-06 22:50:26 +0100

    int2

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+export default {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html' // important for client-side routes
+    }),
+    paths: {
+      // REQUIRED for GitHub Pages when deploying to https://user.github.io/<repo>/
+      base: process.env.BASE_PATH ?? ''
+    }
+  }
 };
-
-export default config;
