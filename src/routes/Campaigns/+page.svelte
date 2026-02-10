13b717a364592b8aced95574632846b12e8e8424
Toon-van-Berkel <toonvanberkel203@gmail.com>
2026-02-10 16:04:00 +0100

    Create +page.svelte

---
+++ src/routes/Campaigns/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { base } from '$app/paths';
+</script>
+
+
+<main>
+    <div class="breadcrumbs">
+        <a href="{base}/">Home</a> /
+        <a href="{base}/Campaigns">Campaigns</a>
+    </div>
+</main>
