c1b62b8e53ae6c9da078a08ef35213b202c927ba
Toon-van-Berkel <toonvanberkel203@gmail.com>
2026-02-10 22:58:55 +0100

    Fixed namespacing

---
+++ src/routes/Campaigns/Main/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<main>
+    <div class="breadcrumbs">
+        <a href="{base}/">Home</a> /
+        <a href="{base}/Campaigns">Campaigns</a> /
+        <a href="{base}/Campaigns/Main">Main</a>
+    </div>
+
+    <hr>
+    
+    <h1>Main</h1>
+
+    <img class="map-main" src="{base}/main/map.png" alt="The map">
+
+    <br>
+
+    <h2>My character</h2>
+    <p>
+        Im playing a rogue, this rogue is named <a href="{base}/Campaigns/Main/Characters/Vaelis Thorne">
+        Vealis Thorne</a>.
+    </p>
+
+    <hr class="light-line">
+    
+    <section class="article">
+        <h2>Links</h2>
+        <ul>
+            <li><a href="{base}/Campaigns/Main/Characters">Characters</a></li>
+            <ul>
+                <li><a href="{base}/Campaigns/Main/Characters/Vaelis Thorne">Vaelis Thorne</a></li>
+            </ul>
+            <li><a href="{base}/Campaigns/Main/Hierarchy">Hierarchy</a></li>
+        </ul>
+    </section>
+</main> 
---
+++ src/routes/Campaigns/Main/Characters/+page.svelte
@@ -0,0 +1,128 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<main>
+    <div class="breadcrumbs">
+        <a href="{base}/">Home</a> /
+        <a href="{base}/Campaigns">Campaigns</a> /
+        <a href="{base}/Campaigns/Main">Main</a> /
+        <a href="{base}/Campaigns/Main/Characters">Characters</a> 
+    </div>
+
+    <hr>
+    
+    <h1>Characters</h1>
+    <div class="cards longcards">
+        <a href="{base}/Campaigns/Main/Characters/Vaelis Thorne">
+            <img src="{base}/main/VaelisThorne.png" alt="camp 1">
+            <div>
+                <h2>Vaelis Thorne</h2>
+                <br>
+                <span><b>Species:</b> Changeling</span>
+                <span><b>Class:</b> Rogue</span>
+                <span><b>Type:</b> Player</span>
+            </div>
+        </a>
+        <a href="{base}/Campaigns/Main/Characters">
+            <img src="{base}/main/vaelis/Marrek Vaal.png" alt="camp 2">
+            <div>
+                <h2>Marrek Vaal</h2>
+                <br>
+                <span><b>Species:</b> Half elf</span>
+                <span><b>Class:</b> ???</span>
+                <span><b>Type:</b> NPC (Backstory)</span>
+            </div>
+        </a>
+        <a href="{base}/Campaigns/Main/Characters">
+            <img src="{base}/main/vaelis/Iscara Morn.png" alt="camp 2">
+            <div>
+                <h2>Iscara Morn</h2>
+                <br>
+                <span><b>Species:</b> Tiefling</span>
+                <span><b>Class:</b> Alchemist</span>
+                <span><b>Type:</b> NPC (Backstory)</span>
+            </div>
+        </a>
+        <a href="{base}/Campaigns/Main/Characters">
+            <img src="{base}/main/vaelis/Helior Quen.png" alt="camp 2">
+            <div>
+                <h2>Helior Quen</h2>
+                <br>
+                <span><b>Species:</b> Dwarf</span>
+                <span><b>Class:</b> Barbarian</span>
+                <span><b>Type:</b> NPC (Backstory)</span>
+            </div>
+        </a>
+        <a href="{base}/Campaigns/Main/Characters">
+            <img src="{base}/main/vaelis/Veska Drin.png" alt="camp 2">
+            <div>
+                <h2>Veska Drin</h2>
+                <br>
+                <span><b>Species:</b> ???</span>
+                <span><b>Class:</b> ???</span>
+                <span><b>Type:</b> NPC (Backstory)</span>
+            </div>
+        </a>
+        <a href="{base}/Campaigns/Main/Characters">
+            <img src="{base}/main/vaelis/Noct Sel.png" alt="camp 2">
+            <div>
+                <h2>Noct Sel</h2>
+                <br>
+                <span><b>Species:</b> Dark elf</span>
+                <span><b>Class:</b> Spy master</span>
+                <span><b>Type:</b> NPC (Backstory)</span>
+            </div>
+        </a>
+        <a href="{base}/Campaigns/Main/Characters">
+            <img src="{base}/main/vaelis/Aurelion Solace.png" alt="camp 2">
+            <div>
+                <h2>Aurelion Solace</h2>
+                <br>
+                <span><b>Species:</b> Dark elf</span>
+                <span><b>Class:</b> Spy master</span>
+                <span><b>Type:</b> NPC (Backstory)</span>
+            </div>
+        </a>
+        <a href="{base}/Campaigns/Main/Characters">
+            <img src="{base}/main/vaelis/Maera Kull.png" alt="camp 2">
+            <div>
+                <h2>Maera Kull</h2>
+                <br>
+                <span><b>Species:</b> Vampire</span>
+                <span><b>Class:</b> Nightbringer</span>
+                <span><b>Type:</b> NPC (Backstory)</span>
+            </div>
+        </a>
+        <a href="{base}/Campaigns/Main/Characters">
+            <img src="{base}/main/vaelis/Ryn Locke.png" alt="camp 2">
+            <div>
+                <h2>Ryn Locke</h2>
+                <br>
+                <span><b>Species:</b> Skull knight</span>
+                <span><b>Class:</b> Warrior commander</span>
+                <span><b>Type:</b> NPC (Backstory)</span>
+            </div>
+        </a>
+        <a href="{base}/Campaigns/Main/Characters">
+            <img src="{base}/main/vaelis/Eldra Veil.png" alt="camp 2">
+            <div>
+                <h2>Eldra Veil</h2>
+                <br>
+                <span><b>Species:</b> Cambion</span>
+                <span><b>Class:</b> Paladin</span>
+                <span><b>Type:</b> NPC (Backstory)</span>
+            </div>
+        </a>
+        <a href="{base}/Campaigns/Main/Characters">
+            <img src="{base}/main/vaelis/Aurelion the Accreted.png" alt="camp 2">
+            <div>
+                <h2>Aurelion the Accreted</h2>
+                <br>
+                <span><b>Species:</b> Demigod</span>
+                <span><b>Class:</b> Unknown</span>
+                <span><b>Type:</b> NPC (Backstory)</span>
+            </div>
+        </a>
+    </div>
+</main>
