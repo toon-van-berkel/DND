4f3a857e59515beb702c5decc6026bf8d229662a
Toon-van-Berkel <toonvanberkel203@gmail.com>
2026-02-10 15:59:52 +0100

    update x2

---
+++ src/routes/Campaigns/ae/Islands/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<main>
+    <div class="breadcrumbs">
+        <a href="{base}/">Home</a> /
+        <a href="{base}/Campaigns">Campaigns</a> /
+        <a href="{base}/Campaigns/ae">Aetherbound</a> /
+        <a href="{base}/Campaigns/ae/Islands">Islands</a>
+    </div>
+</main>
---
+++ src/routes/Campaigns/main/characters/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<main>
+    <div class="breadcrumbs">
+        <a href="{base}/">Home</a> /
+        <a href="{base}/Campaigns">Campaigns</a> /
+        <a href="{base}/Campaigns/main">Main</a> /
+        <a href="{base}/Campaigns/main/characters">Characters</a>
+    </div>
+</main>
